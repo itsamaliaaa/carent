8db983bce0cb1256b833dafcb9e884009a817488
itsamaliaaa <sitia.p24@gmail.com>
2026-05-14 19:46:28 +0700

    add title logo

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>Admin - Carent</title>
+    <link rel="icon" type="image/png" href="{{ asset('images/logo/logo-title.png') }}">
+
 
     {{-- FONT --}}
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
--- resources/views/layouts/customer.blade.php
+++ resources/views/layouts/customer.blade.php
@@ -7,7 +7,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 
     <title>Carent</title>
-    
+    <link rel="icon" type="image/png" href="{{ asset('images/logo/logo-title.png') }}">
+
     @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/search-loading.js'])
 </head>
 <body class="font-inter">
