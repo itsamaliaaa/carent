34d965e87dfe1b640bdfbd8a2fb5ea7e47d28b8c
itsamaliaaa <sitia.p24@gmail.com>
2026-05-19 14:01:18 +0700

    fix klik logo

--- resources/views/components/header/dashboard.blade.php
+++ resources/views/components/header/dashboard.blade.php
@@ -1,3 +1,14 @@
+@php
+    $role = auth()->user()->role;
+    $active = request()->segment(2);
+
+    $activeClass = 'bg-[#E7E9F2] text-[#0B1F67] font-semibold';
+    $hoverClass  = 'text-gray-600 hover:bg-[#E7E9F2] hover:text-[#0B1F67] hover:font-semibold';
+
+    $iconActive = 'brightness-0 saturate-100 invert-[13%] sepia-[61%] saturate-[1400%] hue-rotate-[210deg] brightness-[95%] contrast-[95%]';
+    $iconHover = 'group-hover:brightness-0 group-hover:saturate-100 group-hover:invert-[13%] group-hover:sepia-[61%] group-hover:saturate-[1400%] group-hover:hue-rotate-[210deg] group-hover:brightness-[95%] contrast-[95%]';
+@endphp
+
 <nav class="bg-white border-b px-6 md:px-8 py-3 flex justify-between items-center 
             sticky top-0 z-50">
             
@@ -10,7 +21,19 @@
         </button>
 
         <!-- LOGO -->
-        <img src="{{ asset('images/logo/logo.png') }}" alt="Logo" class="h-9">
+        @if($role == 'super_admin')
+        <a href="{{ route('superadmin.dashboard') }}" class="flex items-center gap-2">
+            <img src="{{ asset('images/logo/logo.png') }}" alt="Logo" class="h-9">
+        </a>
+        @endif
+
+        @if($role == 'admin_rental')
+        <a href="{{ route('admin.dashboard') }}" class="flex items-center gap-2">
+            <img src="{{ asset('images/logo/logo.png') }}" alt="Logo" class="h-9">
+        </a>
+        @endif
+
+
     </div>
 
     <!-- RIGHT: USER -->
