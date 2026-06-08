e825be01dfd67ed24a776bf295d14a8376e25e9a
Riikq <tariqahmad0510@gmail.com>
2026-06-08 09:28:41 +0700

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        if (config('app.env') === 'production'){
+            URL::forceScheme('https');
+        }
         Carbon::setLocale('id');
     }
 }
